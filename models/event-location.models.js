c9b5a6609291dde97a032c85266251c28b7560fe
Vincent <911vincentlee@gmail.com>
2021-09-23 13:36:11 -0600

    Improved database and api routes

--- models/event-location.models.js
+++ models/event-location.models.js
@@ -2,9 +2,15 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const EventLocationSchema = new Schema({
-    event: { type: String, required: true, unique: true},
-    amount: { type: Number, default: 0},
-    location: { type: String, required: true }
+
+    // identical entries will be identified by identical event-location pairs, if an entry is duplicate then the amount counter will go
+    // up by one. This schema layout allows users to search both by events and by locations efficiently, but the downside is there
+    // is less protection against cluttering via duplicate but non identical entries. This will be mitigated with SOME parsing but 
+    // this most likely will not eliminate this drawback
+    event: {type: String, required: true},
+    locations: {type: String, require: true},
+    amount: {type: Number, default: 0},
+
     },
     {
         timestamps: true,
--- routes/data-route.js
+++ routes/data-route.js
@@ -1,17 +1,39 @@
 const router = require("express").Router();
-const axios = require("axios");
+//const axios = require("axios");
 let EventLocation = require('../models/event-location.models');
 
 
-// Upload data to database
+// Create new event
 router.route('/add').post((req, res)=>{
+    let event = req.body.event;
+    let location = req.body.location;
+    EventLocation.findOneAndUpdate(
+    // Filter
+    {
+        event: event,
+        location: location
+    }, 
+    // Update
+    {
+        // Increments amount field by 1
+        $inc: {
+            amount: 1
+        } 
+    },
+    // Params
+    {
+        // If none is found, create new document
+        upsert: true
+    })
+    .then(eventlocation => res.json(eventlocation))
+    .catch(err => res.status(400).json('Error: ' + err))
 
 });
 
 // Get event data from database
 router.route('/getEvent').get((req, res)=>{
     let event = req.body.event;
-    EventLocation.findOne({event: event})
+    EventLocation.find({event: event})
     .then(eventlocation => res.json(eventlocation))
     .catch(err => res.status(400).json('Error: ' + err))
 
@@ -20,10 +42,9 @@ router.route('/getEvent').get((req, res)=>{
 // Get location data from database
 router.route('/getLocation').get((req, res)=>{
     let location = req.body.location;
-    EventLocation.findOne({location: location})
+    EventLocation.find({location: location})
     .then(eventlocation => res.json(eventlocation))
     .catch(err => res.status(400).json('Error: ' + err))
-
 });
 
 module.exports = router;
